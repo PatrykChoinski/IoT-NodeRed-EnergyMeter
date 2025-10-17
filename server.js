935cdfe40fec4ea3ea353c48d8bbb932e5da458d
Patryk Choinski <pchoinski@induprogress.pl>
2025-10-17 14:11:11 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const http = require("http");
-const express = require("express");
-const RED = require("node-red");
+const http = require('http');
+const express = require('express');
+const RED = require('node-red');
 
 const app = express();
 const server = http.createServer(app);
 
 const settings = {
-    httpAdminRoot: "/",
+    httpAdminRoot: "/red",
     httpNodeRoot: "/api",
     userDir: "./.nodered",
-    functionGlobalContext: {}
+    functionGlobalContext: {},
 };
 
 RED.init(server, settings);
-
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Server listening on port " + (process.env.PORT || 3000));
+});
+
 RED.start();
